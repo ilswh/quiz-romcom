e75bc3dbf2ebb11b8745fb5b86dbbfb6edb2860e
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2023-11-15 18:44:18 +0000

    Make score area interactive

--- assets/js/script.js
+++ assets/js/script.js
@@ -149,5 +149,24 @@ const questions = [
         { text: 'U', correct: true }
     ]
   }
+  /**
+ * Gets the current score from the DOM and increments it by 1
+ */
+function incrementScore() {
+
+  let oldScore = parseInt(document.getElementById("score").innerText);
+  document.getElementById("score").innerText = ++oldScore;
+
+}
+
+/**
+* Gets the current tally of incorrect answers from the DOM and increments it by 1
+*/
+function incrementWrongAnswer() {
+
+  let oldScore = parseInt(document.getElementById("incorrect").innerText);
+  document.getElementById("incorrect").innerText = ++oldScore;
+  
+}
 
 ]
