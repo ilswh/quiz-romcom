5d6093b00874ed2f80895c027913cc1acb07c3ba
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2023-11-16 09:18:38 +0000

    Rearrange, rename and: add welcome, scorearea, container to const and function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
+/* jshint esversion: 11 */
 const startButton = document.getElementById('start-btn');
+const welcome = document.getElementById('welcome');
+const scoreArea = document.getElementById('score-area');
+const container = document.getElementById('container');
 const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
@@ -6,81 +10,105 @@ const answerButtonsElement = document.getElementById('answer-buttons');
 
 let shuffledQuestions, currentQuestionIndex;
 
-startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', startGame);
 nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
+
+  setNextQuestion();
+});
 
 function startGame() {
-  startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
-};
+  startButton.classList.add('hide');
+  welcome.classList.add('hide');
+  scoreArea.classList.remove('hide');
+  container.classList.remove('hide');
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion();
+}
 
 function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
-};
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
 
 function showQuestion(question) {
-  questionElement.innerText = question.question
+  questionElement.innerText = question.question;
   question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
+    const button = document.createElement('button');
+    button.innerText = answer.text;
+    button.classList.add('btn');
     if (answer.correct) {
-      button.dataset.correct = answer.correct
+      button.dataset.correct = answer.correct;
     }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
-};
+    button.addEventListener('click', selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
 
 function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
+  clearStatusClass(document.body);
+  nextButton.classList.add('hide');
   while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
-};
+}
 
 function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
+  const selectedButton = e.target;
+  const isCorrect = selectedButton.dataset.correct;
+  setStatusClass(document.body, isCorrect);
+  if (isCorrect) {
+    incrementScore();
+  } else {
+    incrementWrongAnswer();
+  }
   Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
+    setStatusClass(button, button.dataset.correct);
+  });
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
+    nextButton.classList.remove('hide');
   } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
+    startButton.innerText = 'Restart';
+    startButton.classList.remove('hide');
   }
-};
+}
 
-function setStatusClass(element, correct) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add('correct')
+function setStatusClass(button, isCorrect) {
+  clearStatusClass(button);
+  if (isCorrect) {
+    button.classList.add('correct');
   } else {
-    element.classList.add('wrong')
+    button.classList.add('wrong');
   }
-};
+}
 
-function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
-};
+function clearStatusClass(button) {
+  button.classList.remove('correct');
+  button.classList.remove('wrong');
+}
 
 const questions = [{
     question: 'What movie ?',
     answers: [{
         text: 'I',
+        correct: false
+      },
+      {
+        text: 'L',
+        correct: false
+      },
+      {
+        text: 'U',
         correct: true
+      }
+    ]
+  },
+  {
+    question: 'What movie ?',
+    answers: [{
+        text: 'I',
+        correct: false
       },
       {
         text: 'L',
@@ -104,23 +132,7 @@ const questions = [{
       },
       {
         text: 'U',
-        correct: true
-      }
-    ]
-  },
-  {
-    question: 'What movie ?',
-    answers: [{
-        text: 'I',
-        correct: true
-      },
-      {
-        text: 'L',
         correct: false
-      },
-      {
-        text: 'U',
-        correct: true
       }
     ]
   },
@@ -136,7 +148,7 @@ const questions = [{
       },
       {
         text: 'U',
-        correct: true
+        correct: false
       }
     ]
   },
@@ -152,7 +164,7 @@ const questions = [{
       },
       {
         text: 'U',
-        correct: true
+        correct: false
       }
     ]
   },
@@ -160,15 +172,31 @@ const questions = [{
     question: 'Who is her co-star in ...?',
     answers: [{
         text: 'I',
-        correct: true
-      },
-      {
-        text: 'L',
         correct: false
       },
       {
-        text: 'U',
+        text: 'L',
         correct: true
+      },
+      {
+        text: 'U',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: 'Where does the line ... appear?',
+    answers: [{
+        text: 'I',
+        correct: false
+      },
+      {
+        text: 'L',
+        correct: true
+      },
+      {
+        text: 'U',
+        correct: false
       }
     ]
   },
@@ -184,23 +212,7 @@ const questions = [{
       },
       {
         text: 'U',
-        correct: true
-      }
-    ]
-  },
-  {
-    question: 'Where does the line ... appear?',
-    answers: [{
-        text: 'I',
-        correct: true
-      },
-      {
-        text: 'L',
         correct: false
-      },
-      {
-        text: 'U',
-        correct: true
       }
     ]
   },
@@ -208,7 +220,7 @@ const questions = [{
     question: 'Where does the line ... appear?',
     answers: [{
         text: 'I',
-        correct: true
+        correct: false
       },
       {
         text: 'L',
@@ -220,17 +232,16 @@ const questions = [{
       }
     ]
   }
+];
 
-  function incrementScore() {
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
-  }
+function incrementScore() {
+  let oldScore = parseInt(document.getElementById("score").innerText);
+  document.getElementById("score").innerText = ++oldScore;
+}
 
-  function incrementWrongAnswer() {
+function incrementWrongAnswer() {
 
-    let oldScore = parseInt(document.getElementById("incorrect").innerText);
-    document.getElementById("incorrect").innerText = ++oldScore;
+  let oldScore = parseInt(document.getElementById("incorrect").innerText);
+  document.getElementById("incorrect").innerText = ++oldScore;
 
-  }
-
-]
+}
